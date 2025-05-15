26095794dfccbda126afb98c599184bdc67e3a01
thiagoMilitao <thiago.militao2007@gmail.com>
2025-05-15 16:29:39 -0300

    exhorasextras

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿double salarioHora, salario;
+int horasTrabalhadas, horasExtras;
+
+//solicitar entradas
+Console.WriteLine("Digite o salário-hora base:");
+salarioHora = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o número de horas trabalhadas:");
+horasTrabalhadas = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o número de horas extras:");
+horasExtras = int.Parse(Console.ReadLine());
+
+//calcular o salário
+
+salario = CalcularSalario(salarioHora, horasTrabalhadas, horasExtras);
+
+Console.WriteLine($"O salário é: {salario:C2}");
+
+double CalcularSalario(double salarioHora, int horasExtras, int horasTrabalhadas){
+    return  (salarioHora * horasTrabalhadas + (horasExtras * 1.4 * salarioHora));
+}
